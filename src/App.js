d000db8ba29e6dbd59b495e268b828a8960de8f3
Lorenzo <lorenzozemp@icloud.com>
2022-11-27 13:15:00 +1300

    Optimized code

--- src/App.js
+++ src/App.js
@@ -54,178 +54,37 @@ function App() {
 
   //Called on page load to add keydown and keyup event listener to page
   useEffect(() => {
-    console.log("useEffect");
     document.addEventListener("keydown", handleKeyDown, true);
     document.addEventListener("keyup", handleKeyUp, true);
   });
 
   //Handle button clicks
   function handleClick(e) {
-    playAudio(e.target.children[0]);
     setPadName(e.target.children[0].className);
-    e.target.classList.remove("bg-neu-linear-gradient-out");
-    e.target.classList.add("bg-neu-linear-gradient-in");
+    playClip(e.target.id);
+    animateDown(e.target.id);
     setTimeout(() => {
-      e.target.classList.remove("bg-neu-linear-gradient-in");
-      e.target.classList.add("bg-neu-linear-gradient-out");
+      animateUp(e.target.id);
     }, 150);
   }
 
   //Handle key press down
   function handleKeyDown(e) {
-    //console.log(e);
-    switch (e.code) {
-      case "KeyQ":
-        playAudio(document.getElementById("Q").children[0]);
-        document
-          .getElementById("Q")
-          .classList.remove("bg-neu-linear-gradient-out");
-        document.getElementById("Q").classList.add("bg-neu-linear-gradient-in");
-        break;
-      case "KeyW":
-        playAudio(document.getElementById("W").children[0]);
-        document
-          .getElementById("W")
-          .classList.remove("bg-neu-linear-gradient-out");
-        document.getElementById("W").classList.add("bg-neu-linear-gradient-in");
-        break;
-      case "KeyE":
-        playAudio(document.getElementById("E").children[0]);
-        document
-          .getElementById("E")
-          .classList.remove("bg-neu-linear-gradient-out");
-        document.getElementById("E").classList.add("bg-neu-linear-gradient-in");
-        break;
-      case "KeyA":
-        playAudio(document.getElementById("A").children[0]);
-        document
-          .getElementById("A")
-          .classList.remove("bg-neu-linear-gradient-out");
-        document.getElementById("A").classList.add("bg-neu-linear-gradient-in");
-        break;
-      case "KeyS":
-        playAudio(document.getElementById("S").children[0]);
-        document
-          .getElementById("S")
-          .classList.remove("bg-neu-linear-gradient-out");
-        document.getElementById("S").classList.add("bg-neu-linear-gradient-in");
-        break;
-      case "KeyD":
-        playAudio(document.getElementById("D").children[0]);
-        document
-          .getElementById("D")
-          .classList.remove("bg-neu-linear-gradient-out");
-        document.getElementById("D").classList.add("bg-neu-linear-gradient-in");
-        break;
-      case "KeyZ":
-        playAudio(document.getElementById("Z").children[0]);
-        document
-          .getElementById("Z")
-          .classList.remove("bg-neu-linear-gradient-out");
-        document.getElementById("Z").classList.add("bg-neu-linear-gradient-in");
-        break;
-      case "KeyX":
-        playAudio(document.getElementById("X").children[0]);
-        document
-          .getElementById("X")
-          .classList.remove("bg-neu-linear-gradient-out");
-        document.getElementById("X").classList.add("bg-neu-linear-gradient-in");
-        break;
-      case "KeyC":
-        playAudio(document.getElementById("C").children[0]);
-        document
-          .getElementById("C")
-          .classList.remove("bg-neu-linear-gradient-out");
-        document.getElementById("C").classList.add("bg-neu-linear-gradient-in");
-        break;
-      default:
-        break;
-    }
+    setPadName(
+      document.getElementById(e.key.toUpperCase()).children[0].className
+    );
+    playClip(e.key.toUpperCase());
+    animateDown(e.key.toUpperCase());
   }
 
-  //Handle key release up
+  //Handle Key Release
   function handleKeyUp(e) {
-    // console.log(e.beingPressed);
-    switch (e.code) {
-      case "KeyQ":
-        document
-          .getElementById("Q")
-          .classList.add("bg-neu-linear-gradient-out");
-        document
-          .getElementById("Q")
-          .classList.remove("bg-neu-linear-gradient-in");
-        break;
-      case "KeyW":
-        document
-          .getElementById("W")
-          .classList.add("bg-neu-linear-gradient-out");
-        document
-          .getElementById("W")
-          .classList.remove("bg-neu-linear-gradient-in");
-        break;
-      case "KeyE":
-        document
-          .getElementById("E")
-          .classList.add("bg-neu-linear-gradient-out");
-        document
-          .getElementById("E")
-          .classList.remove("bg-neu-linear-gradient-in");
-        break;
-      case "KeyA":
-        document
-          .getElementById("A")
-          .classList.add("bg-neu-linear-gradient-out");
-        document
-          .getElementById("A")
-          .classList.remove("bg-neu-linear-gradient-in");
-        break;
-      case "KeyS":
-        document
-          .getElementById("S")
-          .classList.add("bg-neu-linear-gradient-out");
-        document
-          .getElementById("S")
-          .classList.remove("bg-neu-linear-gradient-in");
-        break;
-      case "KeyD":
-        document
-          .getElementById("D")
-          .classList.add("bg-neu-linear-gradient-out");
-        document
-          .getElementById("D")
-          .classList.remove("bg-neu-linear-gradient-in");
-        break;
-      case "KeyZ":
-        document
-          .getElementById("Z")
-          .classList.add("bg-neu-linear-gradient-out");
-        document
-          .getElementById("Z")
-          .classList.remove("bg-neu-linear-gradient-in");
-        break;
-      case "KeyX":
-        document
-          .getElementById("X")
-          .classList.add("sbg-neu-linear-gradient-out");
-        document
-          .getElementById("X")
-          .classList.remove("bg-neu-linear-gradient-in");
-        break;
-      case "KeyC":
-        document
-          .getElementById("C")
-          .classList.add("bg-neu-linear-gradient-out");
-        document
-          .getElementById("C")
-          .classList.remove("bg-neu-linear-gradient-in");
-        break;
-      default:
-        break;
-    }
+    animateUp(e.key.toUpperCase());
   }
 
-  function playAudio(audioElement) {
-    //IF audio is already playing STOP audio where it is and play it again
+  function playClip(id) {
+    const audioElement = document.getElementById(id).children[0];
+
     if (audioElement.paused === false) {
       audioElement.pause();
       audioElement.currentTime = 0.0;
@@ -234,6 +93,16 @@ function App() {
       //ELSE play normally
       audioElement.play();
     }
+  }
+
+  function animateDown(id) {
+    document.getElementById(id).classList.remove("bg-neu-linear-gradient-out");
+    document.getElementById(id).classList.add("bg-neu-linear-gradient-in");
+  }
+
+  function animateUp(id) {
+    document.getElementById(id).classList.remove("bg-neu-linear-gradient-in");
+    document.getElementById(id).classList.add("bg-neu-linear-gradient-out");
   }
 
   return (
